1cdbb95a2219ba9dd52ed2564827d9fca048b433
Ben <bjbraham@gmail.com>
2020-01-14 01:41:45 +1100

    Add TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar'
 import Sidebar from './Components/Sidebar/Sidebar'
 
+import TodoList from './Modules/TodoList/TodoList'
+
 const App: React.FC = () => {
   const [sidebar, setSidebar] = React.useState(false)
   const toggleSidebar = (): void => setSidebar(!sidebar)
+
+  const [module, setModule] = React.useState(<TodoList />)
   return (
     <div className="App">
       <Navbar toggleSidebar={toggleSidebar} />
       <Sidebar open={sidebar} />
+      { module }
+
       AAAAAAAAAAAAAAAAAAA
       AAAAAAAAAAAAAAAAAAA
       AAAAAAAAAAAAAAAAAAA
---
+++ src/Modules/TodoList/TodoList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TodoList.css'
+
+const TodoList: React.FC = () => {
+  return (
+    <div className='TodoList' >
+      hey
+    </div>
+  )
+}
+
+export default TodoList
