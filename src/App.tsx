eb7fa8ffda2df81523aace61af7f50a6976409e5
Ben <bjbraham@gmail.com>
2020-01-15 01:51:40 +1100

    fix login state

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,20 @@ import Sidebar from './Components/Sidebar/Sidebar'
 import TodoList from './Modules/TodoList/TodoList'
 import firebaseRef from './firebase' 
 
-const onAuthStateChange = () => {
+type user = {
+  loggedIn: boolean
+  id: string
+}
+
+const onAuthStateChange = (setUser: (user: user) => void) => {
   return firebaseRef.auth().onAuthStateChanged(user => {
     if (user) {
       console.log('user logged in')
       console.log(user)
+      setUser({loggedIn: true, id: 'aaa'})
     } else {
       console.log('logged out')
+      setUser({loggedIn: false, id: 'aaa'})
     }
   })
 }
@@ -20,8 +27,13 @@ const App: React.FC = () => {
   const [sidebar, setSidebar] = React.useState(false)
   const toggleSidebar = (): void => setSidebar(!sidebar)
   const [user, setUser] = React.useState({ loggedIn: false, id: 'bbbb' })
-
   const [module, setModule] = React.useState(<TodoList boardId={'oPJb7gIdzFSjU7FQswRS'} moduleId={'KwYx1joUzZbsfESRgh8o'}/>)
+
+  React.useEffect(() => {
+    const unsubscribe = onAuthStateChange(setUser)
+    return () => { unsubscribe() }
+  }, [])
+
   return (
     <div className="App">
       <Navbar toggleSidebar={toggleSidebar} />
