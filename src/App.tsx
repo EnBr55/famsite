c308d6fa7ef61770d95984edbaac4ce191ee31d9
Ben <bjbraham@gmail.com>
2020-01-15 01:51:06 +1100

    Add login state

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
 import Sidebar from './Components/Sidebar/Sidebar'
-
 import TodoList from './Modules/TodoList/TodoList'
+import firebaseRef from './firebase' 
+
+const onAuthStateChange = () => {
+  return firebaseRef.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log('user logged in')
+      console.log(user)
+    } else {
+      console.log('logged out')
+    }
+  })
+}
 
 const App: React.FC = () => {
   const [sidebar, setSidebar] = React.useState(false)
   const toggleSidebar = (): void => setSidebar(!sidebar)
+  const [user, setUser] = React.useState({ loggedIn: false, id: 'bbbb' })
 
   const [module, setModule] = React.useState(<TodoList boardId={'oPJb7gIdzFSjU7FQswRS'} moduleId={'KwYx1joUzZbsfESRgh8o'}/>)
   return (
@@ -20,6 +32,7 @@ const App: React.FC = () => {
       AAAAAAAAAAAAAAAAAAA
       AAAAAAAAAAAAAAAAAAA
       { module }
+      <span>aaaa{user.id}</span>
     </div>
   )
 }
