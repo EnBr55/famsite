5478440a55f7fca8fd167dc145dddf5e88051581
Ben <bjbraham@gmail.com>
2020-01-18 10:31:24 +1100

    Add member list

--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -3,7 +3,7 @@ import './Board.css'
 import firebaseRef from '../../firebase'
 import { SidebarContext } from '../../Contexts/SidebarContext'
 import firebase from 'firebase'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 
 type module = {
   id: string
@@ -52,6 +52,30 @@ const Board: React.FC<props> = ({ setBoard, board, modules }) => {
   const [newUserId, setNewUserId] = React.useState('')
   const [newModuleName, setNewModuleName] = React.useState('')
   const [newModuleType, setNewModuleType] = React.useState('')
+  const [userList, setUserList] = React.useState<string[]>([])
+
+  React.useEffect(() => {
+    const unsubscribe = firebaseRef
+      .firestore()
+      .collection('boards')
+      .doc(board.id)
+      .onSnapshot(snapshot => {
+        const members: string[] = snapshot.data()!.members
+        const memberNames: string[] = []
+        let counter = 0
+        members.forEach(member => {
+          firebaseRef.firestore().collection('users').doc(member).get().then(doc => {
+            memberNames.push(doc.data()!.name)
+            counter++
+            if (counter === members.length) {
+              setUserList(memberNames)
+            }
+          })
+        })
+      })
+    return unsubscribe
+  }, [])
+
   const moduleList = modules.map((module) => (
     <div
       className="module"
@@ -66,7 +90,7 @@ const Board: React.FC<props> = ({ setBoard, board, modules }) => {
       }}
     >
       {module.name}
-    <div className="type-label">[{module.type}]</div>
+      <div className="type-label">[{module.type}]</div>
     </div>
   ))
 
@@ -91,28 +115,30 @@ const Board: React.FC<props> = ({ setBoard, board, modules }) => {
           module: doc.id,
         })
         sidebar.setSidebar(undefined)
-      }
-      )
+      })
       .catch((error) => console.log(error))
   }
 
   return (
     <div className="board">
       <h1>{board.name}</h1>
-      <div className='back' onClick={() => sidebar.setSidebar(sidebar.default)}>
+      <div className="back" onClick={() => sidebar.setSidebar(sidebar.default)}>
         <ArrowBackIcon /> <span> back </span>
       </div>
       {moduleList}
       <br />
       <hr />
       <br />
-      <input placeholder='User Id' onChange={(e) => setNewUserId(e.target.value)} />
-      <button onClick={() => addUserToBoard(board, newUserId)}>Add User</button>
-      <br />
       <br />
       <p>New module</p>
-      <input placeholder='Module Name' onChange={(e) => setNewModuleName(e.target.value)} />
-      <select defaultValue="a" onChange={(e) => setNewModuleType(e.target.value)}>
+      <input
+        placeholder="Module Name"
+        onChange={(e) => setNewModuleName(e.target.value)}
+      />
+      <select
+        defaultValue="a"
+        onChange={(e) => setNewModuleType(e.target.value)}
+      >
         <option disabled value="a">
           Module type
         </option>
@@ -125,6 +151,15 @@ const Board: React.FC<props> = ({ setBoard, board, modules }) => {
       >
         Create New Module
       </button>
+      <br />
+      <br />
+      <input
+        placeholder="User Id"
+        onChange={(e) => setNewUserId(e.target.value)}
+      />
+      <button onClick={() => addUserToBoard(board, newUserId)}>Add User</button>
+      <h3>Members</h3>
+      {userList.map(user => <li key={user}>{user}</li>)}
     </div>
   )
 }
