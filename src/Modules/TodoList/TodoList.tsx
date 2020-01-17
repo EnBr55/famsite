7fa226cf1d35d3fe7548851225b533305cfef64a
Ben <bjbraham@gmail.com>
2020-01-18 04:34:45 +1100

    Sort todos by date

--- src/Modules/TodoList/TodoList.tsx
+++ src/Modules/TodoList/TodoList.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './TodoList.css'
 import FirebaseRef from '../../firebase'
 import Delete from '@material-ui/icons/Delete'
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank'
+import CheckBoxIcon from '@material-ui/icons/CheckBox'
 
 type todo = {
   label: string
   checked: boolean
+  dateAdded: number
   id: string
 }
 
@@ -17,13 +18,10 @@ type props = {
 }
 
 const TodoList: React.FC<props> = ({ boardId, moduleId }) => {
-
-
   const [todos, setTodos] = React.useState<todo[]>([])
   const [title, setTitle] = React.useState('')
 
-  const ref = 
-    FirebaseRef.firestore()
+  const ref = FirebaseRef.firestore()
     .collection('boards')
     .doc(boardId)
     .collection('modules')
@@ -31,18 +29,26 @@ const TodoList: React.FC<props> = ({ boardId, moduleId }) => {
     .collection('data')
 
   React.useEffect(() => {
-    FirebaseRef.firestore().collection('boards').doc(boardId).collection('modules').doc(moduleId).get().then(doc => setTitle(doc.data()!.name || 'Todo List'))
-    const unsubscribe = 
-      ref.onSnapshot( snapshot => {
-        const newTodos: todo[] = []
-        snapshot.forEach(doc => {
-          newTodos.push({label: doc.data().label, checked: doc.data().checked, id: doc.id})
-        }
-      )
-      setTodos(newTodos)
+    FirebaseRef.firestore()
+      .collection('boards')
+      .doc(boardId)
+      .collection('modules')
+      .doc(moduleId)
+      .get()
+      .then((doc) => setTitle(doc.data()!.name || 'Todo List'))
+    const unsubscribe = ref.onSnapshot((snapshot) => {
+      const newTodos: todo[] = []
+      snapshot.forEach((doc) => {
+        newTodos.push({
+          label: doc.data().label,
+          checked: doc.data().checked,
+          id: doc.id,
+          dateAdded: doc.data().dateAdded
+        })
       })
+      setTodos(newTodos)
+    })
     return unsubscribe
-
   }, [boardId, moduleId])
 
   const [newTodoName, setNewTodoName] = React.useState('')
@@ -51,48 +57,50 @@ const TodoList: React.FC<props> = ({ boardId, moduleId }) => {
     ref.add({
       label: label,
       checked: false,
+      dateAdded: new Date().getTime(),
     })
   }
 
   const deleteTodo = (id: string) => {
-    ref.doc(id).delete().catch(error => {
-      console.log('Error deleting doc: '+error)
-    })
+    ref
+      .doc(id)
+      .delete()
+      .catch((error) => {
+        console.log('Error deleting doc: ' + error)
+      })
   }
 
   const toggleTodo = (todo: todo) => {
     ref.doc(todo.id).set({
       ...todo,
-      checked: !todo.checked
+      checked: !todo.checked,
     })
   }
 
+  const sortByDate = (a: todo, b: todo) => {
+   return (a.dateAdded > b.dateAdded) ? 1 : ((b.dateAdded > a.dateAdded) ? -1 : 0) 
+  }
+
   return (
-    <div className="TodoList" >
+    <div className="TodoList">
       <h1>{title}</h1>
-      {todos.map((todo) => (
-      <div className="todo" key={todo.id}>
-        <div className="bin" onClick={() => deleteTodo(todo.id)} >
-          <Delete /> 
+      {todos.sort(sortByDate).map((todo) => (
+        <div className="todo" key={todo.id}>
+          <div className="bin" onClick={() => deleteTodo(todo.id)}>
+            <Delete />
+          </div>
+          <div className="label">{todo.label} </div>
+          <div className="checkbox" onClick={() => toggleTodo(todo)}>
+            {todo.checked ? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon />}
+          </div>
         </div>
-        <div className="label">
-          {todo.label}{' '}
-        </div>
-        <div className="checkbox" onClick={() => toggleTodo(todo)} >
-          { todo.checked ? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon /> } 
-        </div>
-      </div>
       ))}
-      <div className="adding" >
-        <input 
-        onKeyDown={(e) => e.key === 'Enter' && addNewTodo(newTodoName) } 
-        onChange={(e) => setNewTodoName(e.target.value)} 
+      <div className="adding">
+        <input
+          onKeyDown={(e) => e.key === 'Enter' && addNewTodo(newTodoName)}
+          onChange={(e) => setNewTodoName(e.target.value)}
         />
-        <button
-          onClick={() => addNewTodo(newTodoName)}
-        >
-          Add
-        </button>
+        <button onClick={() => addNewTodo(newTodoName)}>Add</button>
       </div>
     </div>
   )
