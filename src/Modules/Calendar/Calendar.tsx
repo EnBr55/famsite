fab24417cf54757a7958e4b3e6c49b04b5f78981
Ben <bjbraham@gmail.com>
2020-02-26 17:30:27 +1100

    Add firestore pushing to calendarEvent creation modal

--- src/Modules/Calendar/Calendar.tsx
+++ src/Modules/Calendar/Calendar.tsx
@@ -4,16 +4,17 @@ import FirebaseRef from '../../firebase'
 import Delete from '@material-ui/icons/Delete'
 import TextInput from '../../Components/TextInput/TextInput'
 import FullscreenModal from '../../Components/FullscreenModal/FullscreenModal'
+import { UserContext } from '../../Contexts/UserContext'
 import { User } from '../../Models/Users'
 
 type calendarEvent = {
-  label: string
   creator?: User
-  assigned: User[]
-  timeCreated: number
-  time: number
+  label: string
   location: string
   description: string
+  timeCreated: number
+  time: number
+  assigned: User[]
   id: string
 }
 
@@ -25,7 +26,7 @@ const defaultCalendarEvent = {
   time: 1,
   location: 'default location',
   description: 'default description',
-  id: 'default id'
+  id: 'default id',
 }
 
 type props = {
@@ -34,6 +35,8 @@ type props = {
 }
 
 const Calendar: React.FC<props> = ({ boardId, moduleId }) => {
+  const user = React.useContext(UserContext)
+
   const [events, setEvents] = React.useState<calendarEvent[]>([])
   const [title, setTitle] = React.useState('')
   const [modal, setModal] = React.useState<JSX.Element | undefined>(undefined)
@@ -77,43 +80,73 @@ const Calendar: React.FC<props> = ({ boardId, moduleId }) => {
     return (
       <div className="add-event-dialog">
         <h2>New Event</h2>
-        <TextInput placeholder={'Event Label'} onChange={setNewEventName} maxHeight={'1.5em'}/> 
+        <TextInput
+          placeholder={'Event Label'}
+          onChange={setNewEventName}
+          maxHeight={'1.5em'}
+        />
         <br />
-        <TextInput placeholder={'Location'} onChange={setNewEventLocation} maxHeight={'1.5em'}/> 
+        <TextInput
+          placeholder={'Location'}
+          onChange={setNewEventLocation}
+          maxHeight={'1.5em'}
+        />
         <br />
-        <TextInput placeholder={'Description'} onChange={setNewEventDescription} maxHeight={'1.5em'}/> 
+        <TextInput
+          placeholder={'Description'}
+          onChange={setNewEventDescription}
+          maxHeight={'1.5em'}
+        />
         <br />
-        <input type='date' onChange={e => setNewEventDate(e.target.value)} />
+        <input type="date" onChange={(e) => setNewEventDate(e.target.value)} />
         <br />
-        <input type='time' onChange={e => setNewEventTime(e.target.value)} />
+        <input type="time" onChange={(e) => setNewEventTime(e.target.value)} />
         <br />
+        <button
+          onClick={() =>
+            AddCalendarEvent({
+              creator: user,
+              label: newEventName,
+              location: newEventLocation,
+              description: newEventDescription,
+              timeCreated: new Date().getTime(),
+              time: 0,
+              assigned: [],
+              id: ''
+            })
+          }
+        >
+          Add Event
+        </button>
       </div>
     )
   }
 
-  const addEvent = () => {
+  const AddCalendarEvent = (newCalendarEvent: calendarEvent) => {
+    console.log(newCalendarEvent)
+    let calendarEventWithoutId = newCalendarEvent
+    delete calendarEventWithoutId.id
+    ref.add(calendarEventWithoutId)
   }
 
-  const deleteEvent = () => {
-  }
+  const deleteCalendarEvent = () => {}
 
   const sortByDate = (a: calendarEvent, b: calendarEvent) => {
-   return (a.timeCreated > b.timeCreated) ? 1 : ((b.timeCreated > a.timeCreated) ? -1 : 0) 
+    return a.timeCreated > b.timeCreated
+      ? 1
+      : b.timeCreated > a.timeCreated
+      ? -1
+      : 0
   }
 
   const listEvents = () => {
-    return (
-      events.sort(sortByDate).map(event => (
-        <div className="event">
-          {event.label}
-        </div>
-      ))
-    )
+    return events
+      .sort(sortByDate)
+      .map((event) => <div className="event">{event.label}</div>)
   }
 
   return (
     <div className="calendar">
-
       {modal && (
         <FullscreenModal element={modal} setModal={setModal} closeable={true} />
       )}
@@ -123,7 +156,7 @@ const Calendar: React.FC<props> = ({ boardId, moduleId }) => {
       <div className="add-event" onClick={() => setModal(addEventDialog())}>
         +
       </div>
-        <h3>{new Date(newEventDate + ' ' + newEventTime).toString()}</h3>
+      <h3>{new Date(newEventDate + ' ' + newEventTime).toString()}</h3>
     </div>
   )
 }
